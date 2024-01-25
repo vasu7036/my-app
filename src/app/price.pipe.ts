d20cb9d545157fb61ab16319013f8d4cc1ec274f
vasu7036 <vasudevareddymothe@gmail.com>
2024-01-25 10:48:34 +0530

    pricechesges

--- src/app/price.pipe.ts
+++ src/app/price.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PricePipe implements PipeTransform {
 
   transform(value: unknown, ...args: unknown[]): unknown {
-    return "Rs. "+value+"/-";
+    return "Rs."+value+"/-";
   }
 
 }
